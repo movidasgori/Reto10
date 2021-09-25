c104d09e3d05ee2a63c34c73cce33724282a5e23
Movidas <webs.gregory@gmail.com>
2021-09-25 21:49:29 +0200

    Reto 10

---
+++ myapi/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .serializers import HeroSerializer
+from .models import Hero
+
+# Create your views here.
+
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Hero.objects.all().order_by('id')
+    serializer_class = HeroSerializer
